33d77fa5b9eaf3e017221f4cdaa2473f67691058
Rachel Kim <rachelkim8084@gmail.com>
2021-03-19 12:13:02 -0700

    added some method comments

--- src/engine.py
+++ src/engine.py
@@ -4,20 +4,35 @@ import sys
 import os
 import textwrap
 
+"""Defines methods that run the main game."""
+
 def type(string: typing.Sequence):
-    wrapped_text = textwrap.wrap(string, 30)
-    for line in wrapped_text:
-        for character in line:
+    """Prints out letters one by one to console."""
+    wrapped_text = textwrap.wrap(string, 30) # Creates a list with each item being no longer than 30 indexes
+    for line in wrapped_text: # For each line,
+        for character in line: # print each character one by one
             sys.stdout.write(character)
             sys.stdout.flush()
-            time.sleep(0.05)
+            time.sleep(0.05) # Waits in between each letter typed (in seconds)
+
 def slow_type(string: typing.Sequence):
+    """Prints out letters one by one to console slowly."""
     wrapped_text = textwrap.wrap(string, 30)
     for line in wrapped_text:
         for character in line:
             sys.stdout.write(character)
             sys.stdout.flush()
-            time.sleep(0.4)
+            time.sleep(0.4) # Waits longer between each letter typed
+
+def custom_type(string: typing.Sequence, speed: int, float):
+    """Prints out letters one by one to console at speed specified."""
+    wrapped_text = textwrap.wrap(string, 30)
+    for line in wrapped_text:
+        for character in line:
+            sys.stdout.write(character)
+            sys.stdout.flush()
+            time.sleep(speed) # Waits whatever time the user specifies
 
 def clear():
+    """Clears the console."""
     os.system('cls' if os.name == 'nt' else 'clear')  # if microsoft, use cls. if linux/mac, use clear.
---
+++ src/story.py
@@ -0,0 +1,11 @@
+
+""""""
+
+areas = {
+
+}
+
+map = [store, plaza, alley,
+       garden, gates, home,
+       a1, a2, a3,
+       a4, a5, a6]
