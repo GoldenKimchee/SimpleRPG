70eaf187ac31689ae4e438446349a269ab6db2ef
Rachel Kim <rachelkim8084@gmail.com>
2021-03-21 21:15:49 -0700

    added content

--- src/engine.py
+++ src/engine.py
@@ -6,36 +6,50 @@ import textwrap
 
 """Defines methods that run the main game."""
 
-def type(string: typing.Sequence):
+def type(string: typing.Sequence, width: int = 30):
     """Prints out letters one by one to console."""
-    wrapped_text = textwrap.wrap(string, 30) # Creates a list with each item being no longer than 30 indexes
+    wrapped_text = textwrap.wrap(string, width) # Creates a list with each item being no longer than 30 indexes
     for line in wrapped_text: # For each line,
         for character in line: # print each character one by one
             sys.stdout.write(character)
             sys.stdout.flush()
+            if character in ["!", ".", ",", "?"]:
+                time.sleep(0.3)
             time.sleep(0.05) # Waits in between each letter typed (in seconds)
-        print()
+        print() # since textwrap.wrap does not include \n
 
-def slow_type(string: typing.Sequence):
+def slow_type(string: typing.Sequence, width: int = 30):
     """Prints out letters one by one to console slowly."""
-    wrapped_text = textwrap.wrap(string, 30)
+    wrapped_text = textwrap.wrap(string, width)
     for line in wrapped_text:
         for character in line:
             sys.stdout.write(character)
             sys.stdout.flush()
+            if character in ["!", ".", ",", "?"]:
+                time.sleep(0.3)
             time.sleep(0.3) # Waits longer between each letter typed
         print()
 
-def custom_type(string: typing.Sequence, speed: int, float):
+def custom_type(string: typing.Sequence, speed: int, float, pause: int = 0.3, width: int = 30):
     """Prints out letters one by one to console at speed specified."""
-    wrapped_text = textwrap.wrap(string, 30)
+    wrapped_text = textwrap.wrap(string, width)
     for line in wrapped_text:
         for character in line:
             sys.stdout.write(character)
             sys.stdout.flush()
+            if character in ["!", ".", ",", "?"]:
+                time.sleep(pause)
             time.sleep(speed) # Waits whatever time the user specifies
         print()
 
 def clear():
     """Clears the console."""
     os.system('cls' if os.name == 'nt' else 'clear')  # if microsoft, use cls. if linux/mac, use clear.
+
+def get_input(responses: list) -> str:
+    """responses will be a list of options, gets input until it matches an option"""
+    user_response = ""
+    while user_response.strip() not in responses: # continue prompting input until it matches a response
+        print("> ", end= "")
+        user_response = input()
+    return user_response
--- src/story.py
+++ src/story.py
@@ -1,38 +1,45 @@
+import engine
 
 """"""
 
 class Area:
-       def __init__(self, name: str, description: str, difficulty: int):
-              self.name = name
-              self.description = description
-              self.difficulty = difficulty
+    def __init__(self, name: str, description: str, difficulty: int):
+        self.name = name
+        self.description = description
+        self.difficulty = difficulty
 
-       def display(self):
-              print("#" * len(self.name) + 2)
-              print("#" + self.name + "#")
-              print("#" * len(self.name) + 2)
+    def display(self):
+        print("=" * 44)
+        spaces = int((44 - (len(self.name) + 2)) / 2)
+        print("|" + (" " * spaces) + self.name + (" " * spaces)+ "|")
+        print("=" * 44)
+        engine.type(self.description, 44)
 
-
-start = "##########################" \
-        "####### SimpleRPG ########" \
-        "##########################" \
-        "        + Start           " \
-        "        + Load            " \
-        "        + Quit            "
+start = "#############################################\n" \
+        "+                 SimpleRPG                 +\n" \
+        "#############################################\n" \
+        "                   + Start                   \n" \
+        "                   + Load                    \n" \
+        "                   + Quit                    \n"
 
 # name        | the area's name will be always displayed while the player is there
 # description | gives the player a basic description of the area
-# observe     | an action the player can 
-# events      |
+# observe     | an action the player can choose, gives more info about the area
+
 a1 = {'name': 'Lake',
-      'description': 'An open lake is in the middle of an ocean of trees.',
-      'observe': 'Taking a closer look at the lake, you sense that the lake is far deeper than you expected.',
+      'description': "An open lake is in the middle of an ocean of trees. As the waters move gently across, " +
+                     "you think the glitter of sunlight reflected off the waves is quite pretty.",
+      'observe': ["Taking a closer look at the lake, you curiously peer down into the lake's blue...",
+                  "Upon that brief inspection, you sense that the lake is far deeper than you expected."],
       'events': [6]}
 
-story = {1: [''], 6: ["A woman stands in the corner "]}
+story = {1: [''], }
 
-
+available_events = []
+triggered_events = []
+"""
 map = [store, plaza, alley,
        garden, gates, home,
        a1, a2, a3,
        a4, a5, a6]
+"""
